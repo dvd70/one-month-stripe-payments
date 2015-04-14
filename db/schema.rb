d734bc1858154d51fd60fd27f6e608c1a3bf93af
dvd70 <dvd70@me.com>
2015-04-14 17:32:30 +0200

    Added the ability to store purchases and display your purchase localy

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,15 @@
 ActiveRecord::Schema.define(version: 20150414135650) do
 
   create_table "purchases", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.integer  "amount"
+    t.string   "description"
+    t.string   "currency"
+    t.string   "customer_id"
+    t.string   "card"
+    t.string   "product_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
